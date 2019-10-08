1a2a7065291024df2f8908c42e7e1f059d70f75a
SouadHasnae <souadasroubi@gmail.com>
2019-10-08 17:54:46 +0100

    msg

--- code.js
+++ code.js
@@ -87,24 +87,44 @@ let s=x+y;
 // 		console.log();
 // 	}
 // }
-function vect(x,y){
-	this.x=x;
-	this.y=y;
+// function vect(x,y){
+// 	this.x=x;
+// 	this.y=y;
 
+// };
+// vect.prototype.module=function(){
+// return Math.sqrt(this.x*this.x+this.y*this.y);
+// }
+// vect.prototype.egale=function(v){
+// 	return (this.x==v.x && this.y==v.y);
+	
+// }
+// let v1=new vect(2,4);
+// let v2=new vect(3,4);
+// console.log (v1.module());
+// console.log(v2.module());
+// console.log(v1.egale(v2));
+
+class vect{
+
+	constructor(x, y) {
+    this.x = x;
+    this.y = y;
 };
-vect.prototype.module=function(){
-return Math.sqrt(this.x*this.x+this.y*this.y);
+
+module(){
+	return  Math.sqrt(this.x*this.x+this.y*this.y);
 }
-vect.prototype.egale=function(v){
+egale(v){
 	return (this.x==v.x && this.y==v.y);
 	
+}
 }
 let v1=new vect(2,4);
 let v2=new vect(3,4);
 console.log (v1.module());
 console.log(v2.module());
 console.log(v1.egale(v2));
-
 
 
 
